bfdb047ea02c1dbe675bf666d57cdb397a08aa82
Dahami Nethsarani <dahaminethsarani2000@gmail.com>
2024-10-07 10:41:15 +0530

    create charts page

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Kpi from './pages/kpi/Kpi'
 import Elec from './pages/elec/Elec';
 /* import Profile from './pages/profile/Profile'; */
 import Mypage from './pages/mypage/Mypage'
+import Tcontent from './pages/content/Content';
 
 export default function App() {
   return (
@@ -31,6 +32,7 @@ export default function App() {
           <Route path="kpi" element={<Kpi />} />
  {/*          <Route path="profile" element={<Profile />} /> */}
           <Route path="mypage" element={<Mypage />} />
+          <Route path="content" element={<Tcontent />} />
 
           {/* Add more routes as needed */}
         </Routes>
