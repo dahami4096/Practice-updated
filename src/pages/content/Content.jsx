6be59eb51c654b0a222f36846c413573b055aa89
Dahami Nethsarani <dahaminethsarani2000@gmail.com>
2024-10-07 12:26:15 +0530

    updated elec page

--- src/pages/content/Content.jsx
+++ src/pages/content/Content.jsx
@@ -69,35 +69,35 @@ function Content() {
   return (
     <main className="main-container">
       <div className="main-title">
-        <h3>DASHBOARD</h3>
+      <a href="/" className="dashboard-link">Dashboard</a>
       </div>
 
-      <div className="main-cards">
-        <div className="card">
-          <div className="card-inner">
+      <div className="tmain-cards">
+        <div className="tcard">
+          <div className="tcard-inner">
             <h3>PRODUCTS</h3>
-            <BsFillArchiveFill className="card_icon" />
+            <BsFillArchiveFill className="tcard_icon" />
           </div>
           <h1>300</h1>
         </div>
-        <div className="card">
-          <div className="card-inner">
+        <div className="tcard">
+          <div className="tcard-inner">
             <h3>CATEGORIES</h3>
-            <BsFillGrid3X3GapFill className="card_icon" />
+            <BsFillGrid3X3GapFill className="tcard_icon" />
           </div>
           <h1>12</h1>
         </div>
-        <div className="card">
-          <div className="card-inner">
+        <div className="tcard">
+          <div className="tcard-inner">
             <h3>CUSTOMERS</h3>
-            <BsPeopleFill className="card_icon" />
+            <BsPeopleFill className="tcard_icon" />
           </div>
           <h1>33</h1>
         </div>
-        <div className="card">
-          <div className="card-inner">
+        <div className="tcard">
+          <div className="tcard-inner">
             <h3>ALERTS</h3>
-            <BsFillBellFill className="card_icon" />
+            <BsFillBellFill className="tcard_icon" />
           </div>
           <h1>42</h1>
         </div>
